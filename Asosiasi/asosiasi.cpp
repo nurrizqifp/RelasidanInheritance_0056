82bf931e0b3a1fb8e17f47449154e54eb7f14232
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-05-27 16:11:09 +0700

    Mengisi prosedur cetakdokter

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -38,3 +38,12 @@ class dokter {
 void pasien::tambahDokter(dokter* pDokter) {
     daftar_dokter.push_back(pDokter);
 }
+
+void pasien::cetakDokter() {
+    cout << "Daftar fokter yang menangani pasien \"" << this->nama << "\":\n";
+    //auto digunakan dalam perulangan
+    for (auto& a : daftar_dokter) {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
